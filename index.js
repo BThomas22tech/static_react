2b88f96c8d6718a05d421e4c570e3af006c3765c
Brandon Thomas <bran_thom@yahoo.com>
2023-04-27 16:59:12 -0500

    vite+react template

--- index.js
+++ index.js
@@ -15,18 +15,18 @@ Hints:
   just like in HTML. In the slide, I have it set to 40px
  */
 
-const page =(
-  <div>
-  <img src="./react-logo.png" width = "40px"/>
-  <h1>Fun Facts about React</h1>
-  <ul>
-  <li>Was first released in 2013</li>
-  <li>Was originally created by Jordan Walke</li>
-  <li>Has over 100k stars on Github</li>
-  <li>Is maintained by Facebook</li>
-  <li>Powers thousands of enterprise apps, including mobile apps</li>
-  </ul>
-  </div>
-)
-
-ReactDOM.render(page, document.getElementById("root"))
+  const page =(
+    <div>
+    <img src="./react-logo.png" width = "40px"/>
+    <h1>Fun Facts about React</h1>
+    <ul>
+    <li>Was first released in 2013</li>
+    <li>Was originally created by Jordan Walke</li>
+    <li>Has over 100k stars on Github</li>
+    <li>Is maintained by Facebook</li>
+    <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
+    </div>
+  )
+  
+  ReactDOM.render(page).document.getElementById("root")
