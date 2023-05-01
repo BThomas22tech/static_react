d1fb44db589a500b2d29ae1ee48fdf090e3ceabd
Brandon Thomas <bran_thom@yahoo.com>
2023-05-01 13:23:45 -0500

    react side logo added

--- react-vite/src/assets/MainContent.jsx
+++ react-vite/src/assets/MainContent.jsx
@@ -2,16 +2,16 @@ import '../App.css'
 
 function MainContent() {
     return (
-    <div className="main-color">
-    <h1>Fun facts about React</h1>
-    <ul>
+    <main className="main-color">
+    <h1 ClassName="fact--title">Fun facts about React</h1>
+    <ul className='main--facts'>
         <li>Was first released in 2013</li>
         <li>Was originally created by Jordan Walke</li>
         <li>Has over 100k stars on Github</li>
         <li>Is maintained by Facebook</li>
         <li>Powers thousands of enterprise apps, including mobile apps</li>
     </ul>
-    </div>
+    </main>
     
     )
 }
