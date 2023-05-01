b548af2223186abf7b3777ece22d433403ea032f
Brandon Thomas <bran_thom@yahoo.com>
2023-05-01 12:41:18 -0500

    main content added

---
+++ react-vite/src/assets/MainContent.jsx
@@ -0,0 +1,19 @@
+import '../App.css'
+
+function MainContent() {
+    return (
+    <div className="main-color">
+    <h1>Fun facts about React</h1>
+    <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has over 100k stars on Github</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
+    </div>
+    
+    )
+}
+
+export default MainContent
--- react-vite/src/main.jsx
+++ react-vite/src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import NavBar from './Navbar.jsx'
+import MainContent from './assets/MainContent.jsx'
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <div className="container">
     <NavBar />
-  </div>,
+    <MainContent />
+  </div>
 )
